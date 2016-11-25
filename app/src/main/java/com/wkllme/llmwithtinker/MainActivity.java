d1ad555b53ba2a85613c9d60f0adc7aa87bbb36c
Haibin <zhaohaibin@wkllme.com>
2016-11-25 14:17:06 +0800

    加载补丁代码

--- app/src/main/java/com/wkllme/llmwithtinker/MainActivity.java
+++ app/src/main/java/com/wkllme/llmwithtinker/MainActivity.java
@@ -1,7 +1,12 @@
 package com.wkllme.llmwithtinker;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loadPatch = (Button) findViewById(R.id.loadPatch);
+        loadPatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                        Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            }
+        });
     }
 }
